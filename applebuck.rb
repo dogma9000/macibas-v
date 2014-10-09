aa1482fcbf142d29b4e83e6da0b13fac88a7c3a2
dogma9000 <artyom.maksimenko@inbox.lv>
2014-10-09 16:49:36 +0300

    applebuck

--- applebuck.rb
+++ applebuck.rb
@@ -1,4 +1,10 @@
-a = 1
-b = 4
-c = 7-1
-d = 45+35
+cenas_latos = [2.32, 4.34, 5.44]
+cenas_euro = []
+
+cenas_latos.each do |cena|
+	cenas_euro << (cena/0.72804).round(3)
+end
+
+cenas_euro.each do |euro_cena|
+	puts euro_cena.to_s+"eur"
+end
